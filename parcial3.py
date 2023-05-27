3a3ef103eb8a4a8d40663a9a53c453f7f7532d61
Raul Pineda <85385878+pineda24@users.noreply.github.com>
2023-05-27 09:19:30 +0200

    edit words

--- parcial3.py
+++ parcial3.py
@@ -2,7 +2,7 @@ import ply.lex as lex
 import ply.yacc as yacc
 import streamlit as st
 
-# Configuración de la página de Streamlit
+# Nombre de la pagina en streamlit
 st.set_page_config(
     page_title="Parcial 3 - COMPILADORES E INTERPRESES"
 )
@@ -25,15 +25,15 @@ t_PARIZQ = r'\('
 t_PARDER = r'\)'
 t_EOL = r'\n'
 
-#  La funcion para el token NUME que si iplica valor
+#  La función para reconocer números enteros y decimales
 def t_NUME(t):
     r'[0-9]+\.[0-9]+|[0-9]+'
     t.value = float(t.value)
     return t
 
-# Función para manejar errores lexicos
+# Función para manejar errores léxicos
 def t_error(t):
-    st.error("Hubo un error lexico...")
+    st.error("Hubo un error léxico...")
     
 # La regla (S -> E EOL)
 def p_s(p):
@@ -41,7 +41,7 @@ def p_s(p):
     p[0] = p[1]
     print(p[0])
 
-## La funcion para las reglas (E -> E + T y E -> E - T)
+## La función para las reglas (E -> E + T y E -> E - T)
 ## Calcula cuando hay una suma o resta
 def p_e1(p):
     '''e : e SUMA t
@@ -51,13 +51,13 @@ def p_e1(p):
     else:
         p[0] = p[1] - p[3]
 
-## La funcion que implementa la regla (E -> T)
+## La función que implementa la regla (E -> T)
 def p_e2(p):
     '''e : t'''
     p[0] = p[1]
 
-## La funcion para las reglas (T -> T * F y T -> T / F)
-## Calcula cuando hay una multiplicacion o divicion
+## La función para las reglas (T -> T * F y T -> T / F)
+## Calcula cuando hay una multiplicacion o division
 def p_t1(p):
     '''
     t : t MULT f
@@ -73,8 +73,8 @@ def p_t2(p):
     '''t : f'''
     p[0] = p[1]
 
-## Funcion que implementa F -> ( E )
-## Calcula cuando se abre un parentesis izquierdo y se cierra con uno derecho
+## Función que implementa F -> ( E )
+## Calcula cuando se abre un paréntesis izquierdo y se cierra con uno derecho
 def p_f1(p):
     '''f : PARIZQ e PARDER'''
     p[0] = p[2]
@@ -84,21 +84,21 @@ def p_f2(p):
     '''f : NUME'''
     p[0] = p[1]
 
-## Para manejar errores sintacticos
+## Para manejar errores sintácticos
 def p_error(p):
     if p:
         st.error("Error de sintaxis en el símbolo:", p.value)
     else:
         st.error("Error de sintaxis en la entrada")
 
-# Definicion de variables del lexer y parser
+# Definición de variables del lexer y parser
 lexer = lex.lex()
 parser = yacc.yacc()
 
 # Captura de la expresion del usuario
 entrada = st.text_input("Expresión ") + '\n'
 
-# Boton para calcular el resultado de la expresion si es correcta la expresion
+# Botón para calcular el resultado de la expresión si es correcta la expresión
 if st.button("Calcular"):
     if parser.parse(entrada, lexer=lexer):
         st.success(f'{entrada} = {parser.parse(entrada, lexer=lexer):.3f}')
