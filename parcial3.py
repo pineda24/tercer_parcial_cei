0ced32c3b8a9eb8f6b70960b207168ef0ee9ddf8
Raul Pineda <85385878+pineda24@users.noreply.github.com>
2023-05-27 08:30:11 +0200

    parcial3 finish

--- parcial3.py
+++ parcial3.py
@@ -25,23 +25,24 @@ t_PARIZQ = r'\('
 t_PARDER = r'\)'
 t_EOL = r'\n'
 
-# Función para tokenizar números
+#  La funcion para el token NUME que si iplica valor
 def t_NUME(t):
     r'[0-9]+\.[0-9]+|[0-9]+'
     t.value = float(t.value)
     return t
 
-# Función de error para manejar caracteres no reconocidos
+# Función para manejar errores lexicos
 def t_error(t):
     st.error("Hubo un error lexico...")
     
-# Definición de la regla inicial de la gramática
+# La regla (S -> E EOL)
 def p_s(p):
     '''s : e EOL'''
     p[0] = p[1]
     print(p[0])
 
-# Reglas de la gramática para la expresión aritmética
+## La funcion para las reglas (E -> E + T y E -> E - T)
+## Calcula cuando hay una suma o resta
 def p_e1(p):
     '''e : e SUMA t
          | e RESTA t'''
@@ -50,10 +51,13 @@ def p_e1(p):
     else:
         p[0] = p[1] - p[3]
 
+## La funcion que implementa la regla (E -> T)
 def p_e2(p):
     '''e : t'''
     p[0] = p[1]
 
+## La funcion para las reglas (T -> T * F y T -> T / F)
+## Calcula cuando hay una multiplicacion o divicion
 def p_t1(p):
     '''
     t : t MULT f
@@ -64,33 +68,37 @@ def p_t1(p):
     else:
         p[0] = p[1] / p[3]
 
+## Esta función implementa (T -> F)
 def p_t2(p):
     '''t : f'''
     p[0] = p[1]
 
+## Funcion que implementa F -> ( E )
+## Calcula cuando se abre un parentesis izquierdo y se cierra con uno derecho
 def p_f1(p):
     '''f : PARIZQ e PARDER'''
     p[0] = p[2]
 
+## Esta función implementa F -> nunme
 def p_f2(p):
     '''f : NUME'''
     p[0] = p[1]
 
+## Para manejar errores sintacticos
 def p_error(p):
     if p:
-        print("Error de sintaxis en el símbolo:", p.value)
+        st.error("Error de sintaxis en el símbolo:", p.value)
     else:
-        print("Error de sintaxis en la entrada")
+        st.error("Error de sintaxis en la entrada")
 
-# Construcción del lexer y parser
+# Definicion de variables del lexer y parser
 lexer = lex.lex()
 parser = yacc.yacc()
 
-# Captura de la entrada del usuario
-entrada = st.text_input("Expresión ")
-entrada += '\n'
+# Captura de la expresion del usuario
+entrada = st.text_input("Expresión ") + '\n'
 
-# Análisis de la expresión y cálculo del resultado
+# Boton para calcular el resultado de la expresion si es correcta la expresion
 if st.button("Calcular"):
     if parser.parse(entrada, lexer=lexer):
         st.success(f'{entrada} = {parser.parse(entrada, lexer=lexer):.3f}')
